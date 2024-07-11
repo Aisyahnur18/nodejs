90cca07b5392eb2dc785ed587c03a8afea9b298d
Aisyah Nur Hasunah <22572005@stmik.jayakarta.ac.id>
2024-07-11 22:17:12 +0700

    update terbaru

--- index.js
+++ index.js
@@ -77,33 +77,18 @@ app.get('/api/movies', (req, res) => {
     });
 });
 
-// delete data
-app.delete('/api/mahasiswa/:nim', (req, res) => {
-    // buat query sql untuk mencari data dan hapus
-    const querySearch = 'SELECT * FROM movies WHERE id = ?';
-    const queryDelete = 'DELETE FROM movies WHERE id = ?';
+// delete data / delete data
+app.delete('/api/movies/:id', (req, res) => {
+    const id = req.params.id;
+    const querySql = 'DELETE FROM movies WHERE id = ?';
 
-    // jalankan query untuk melakukan pencarian data
-    koneksi.query(querySearch, req.params.nim, (err, rows, field) => {
-        // error handling
+    koneksi.query(querySql, [id], (err, rows, field) => {
         if (err) {
             return res.status(500).json({ message: 'Ada kesalahan', error: err });
         }
 
-        // jika id yang dimasukkan sesuai dengan data yang ada di db
-        if (rows.length) {
-            // jalankan query delete
-            koneksi.query(queryDelete, req.params.nim, (err, rows, field) => {
-                // error handling
-                if (err) {
-                    return res.status(500).json({ message: 'Ada kesalahan', error: err });
-                }
-                // jika delete berhasil
-                res.status(200).json({ success: true, message: 'Berhasil hapus data!' });
-            });
-        } else {
-            return res.status(404).json({ message: 'Data tidak ditemukan!', success: false });
-        }
+        // jika request berhasil
+        res.status(200).json({ success: true, message: 'Data berhasil dihapus' });
     });
 });
 
